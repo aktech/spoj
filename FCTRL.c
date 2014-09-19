183d6262c0935b6278508561b264428e66c7f7e4
iaktech <dtu.amit@gmail.com>
2014-09-19 09:24:54 +0530

    FCTRL Formatting

--- FCTRL.c
+++ FCTRL.c
@@ -18,12 +18,11 @@ int main()
 	{   k = 0;
 	    i=1;
 		scanf("%d",&n);
-		while(n >= pow(5,i))
-		{
-			k = k + (n)/(pow(5,i));
+		while(n >= pow(5,i)) {
+        	k = k + (n)/(pow(5,i));
 			i++;
 			}
 			printf("%d\n", k);
 		}
-		return 0;
+	return 0;
 }
